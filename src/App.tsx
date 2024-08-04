54c3584d2f92148c1bbb66fca6a9ad284b263a3c
Gurban Mamedov <138711747+gurbanmamedov@users.noreply.github.com>
2024-08-04 22:54:12 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 
 import './index.css'
 import Header from './components/Header/Header'
-import React from 'react'
 import Hero from './components/Hero/Hero'
 import Benefits from './components/Benefits/Benefits'
 import Footer from './components/Footer/Footer'
