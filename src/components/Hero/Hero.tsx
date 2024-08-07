01e496afe5bb503eda923f179a750e465788c42d
Gurban Mamedov <mike18051996@gmail.com>
2024-08-07 22:13:04 +0400

    changed hero height

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
                     src={bgImage}
                     alt="Background"
                     className="absolute inset-0 w-full h-full object-cover md:object-cover md:w-full md:h-full"
-                    style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }}
+                    style={{ maxWidth: '100%', maxHeight: '100dvh', objectFit: 'cover' }}
                 />
                 <div className="absolute inset-0 bg-gradient-to-b from-[rgb(36,43,51)_4.979%] via-[rgba(45,52,60,0.38)_51.042%] to-[#242B33_86.188%]"></div>
                 <div className="absolute inset-0 bg-gradient-to-t from-[#242B33] to-transparent opacity-60"></div>
