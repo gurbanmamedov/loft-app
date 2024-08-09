617febd4b571deee7c2a30f009f29b8bc508d392
Gurban Mamedov <138711747+gurbanmamedov@users.noreply.github.com>
2024-08-09 06:22:41 +0400

    Update Cards.tsx

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -12,17 +12,17 @@ const Cards = () => {
             Наши квартиры
           </h2>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2  gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {[apartment1, apartment2, apartment3, apartment4].map((src, index) => (
-            <div key={index} className="rounded-lg overflow-hidden shadow-lg relative will-change-transform">
-              <img
-                src={src}
-                alt={`Residence ${index + 1}`}
-                width={540}
-                height={320}
-                className="w-full object-cover"
-                style={{ maxWidth: '100%', maxHeight: '320px' }}
-              />
+            <div key={index} className="rounded-lg overflow-hidden shadow-lg relative">
+              <div className="w-full h-80 md:h-64 lg:h-72 bg-gray-200">
+                <img
+                  src={src}
+                  alt={`Residence ${index + 1}`}
+                  className="w-full h-full object-cover"
+                  style={{ maxWidth: '100%', height: '100%' }}
+                />
+              </div>
               <div className="absolute bottom-0 left-0 right-0 bg-gray-800/90 text-white p-2 md:p-2.5 lg:p-4">
                 <div className="text-lg font-semibold">
                   {index === 0 && 'Пентхаус “Loft Олимп”'}
