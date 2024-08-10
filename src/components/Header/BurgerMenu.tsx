df1e134a7c9e0d096ed3edef4c916c15590f5c3a
Gurban Mamedov <mike18051996@gmail.com>
2024-08-11 03:14:17 +0400

    added burger menu functional

--- src/components/Header/BurgerMenu.tsx
+++ src/components/Header/BurgerMenu.tsx
@@ -1,9 +1,48 @@
-import { Menu } from 'lucide-react';
+import React from "react";
+import {
+    Drawer,
+    DrawerTrigger,
+    DrawerContent,
+    DrawerHeader,
 
-const BurgerMenu = () => {
+} from "../ui/drawer";
+import { MenuIcon } from "lucide-react";
+import logo from '../../assets/logo.svg'
+import { FaInstagram } from "react-icons/fa";
+
+const BurgerMenu: React.FC = () => {
     return (
-        <Menu className='text-white md:hidden' />
-    )
-}
+        <Drawer direction="right">
+            <DrawerTrigger>
+                <div className="text-white"><MenuIcon /></div>
+            </DrawerTrigger>
+            <DrawerContent>
+                <div className="inline-flex self-end p-4">
+                    <DrawerHeader>
+                        <img src={logo} width={120} height={60} />
+                    </DrawerHeader>
+                </div>
+                <div className="flex flex-col pr-8 ">
+                    <ul className="flex flex-col items-end gap-5 text-golden mb-6">
+                        <li><a href="#about" aria-label="О нас">О нас</a></li>
+                        <li><a href="#apartments">Наши Квартиры</a></li>
+                        <li><a href="#">Ипотека</a></li>
+                        <li><a href="#about">Район</a></li>
+                        <li><a href="#about">Контакты</a></li>
+                    </ul>
 
-export default BurgerMenu
+                    <ul className="flex justify-end gap-5 text-golden">
+                        <li><FaInstagram size={30} /></li>
+                        <li><FaInstagram size={30} /></li>
+                        <li><FaInstagram size={30} /></li>
+                        <li><FaInstagram size={30} /></li>
+                    </ul>
+                </div>
+
+
+            </DrawerContent>
+        </Drawer>
+    );
+};
+
+export default BurgerMenu;
