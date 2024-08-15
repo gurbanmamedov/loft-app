f9bb03dfba7d3ad83c53104397cd8f05f16e78b0
Gurban Mamedov <mike18051996@gmail.com>
2024-08-15 15:12:52 +0400

    changed preloader animation

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,9 +1,12 @@
-import ClipLoader from 'react-spinners/ClipLoader';
+
+import SyncLoader from 'react-spinners/SyncLoader';
+import logo from '../../assets/logo.svg'
 
 const Loader = () => {
     return (
-        <div className="flex justify-center items-center h-screen ">
-            <ClipLoader color="#D4C17F" size={60} />
+        <div className="flex justify-center flex-col gap-5 items-center h-screen ">
+            <img src={logo} alt='logo' />
+            <SyncLoader color="#D4C17F" size={20} margin={5} />
         </div>
     );
 };
